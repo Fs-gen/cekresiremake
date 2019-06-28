5fcbfc0fcda81df5c9e6c88ad3d27a9ad137a326
fs-gen <fsgeniuses@gmail.com>
2019-06-28 21:13:54 +0800

    manta

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ error_reporting(0);
 
 <title>CEK RESI</title>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link rel="stylesheet" href="../../css/bootstrap.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
@@ -90,17 +90,15 @@ $("#iloading").hide();
 <body>
 
 
-<div class="container">
+<div class="container mt-3">
 
-
-<h1>CEK RESI</h1>
+<h1 class="font-weight-light">LACAK RESI</h1>
 <hr>
 
 <form class="search" method="POST" name="formku2" id="formku2">
-<input class="search-input form-control" type="search" name="resi" id="resi" placeholder="Nomor Resi..." value="" autofocus="" required="">
-<br>
+<input class="search-input form-control m-1" type="search" name="resi" id="resi" placeholder="Nomor Resi..." value="" autofocus="" required="required">
 <div class="row">
-	<div class="col-md-3">
+	<div class="col-md-3 m-1">
 
 		<select name="jasa" id="jasa" class="search-select form-control" required>
 			<option value="jne">JNE</option>
@@ -110,7 +108,7 @@ $("#iloading").hide();
 		</select>
 	</div>
 
-	<div class="col-md-3">
+	<div class="col-md-3 m-1">
 		<button class="search-submit btn btn-primary" type="submit" name="cek" id="cek">Cek Resi</button>
 	</div>
 </div>
@@ -127,7 +125,7 @@ $("#iloading").hide();
 
 <div>
 
-<div id="ihasil"></div>
+<div style='font-size:10px;' id="ihasil"></div>
 
 </div>
 
